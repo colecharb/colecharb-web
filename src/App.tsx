495d34cd89ac17c788c622115b30e23ef7b1ae48
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-20 12:56:44 -0500

    initialized basic vite + react project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Cole Charbonneau</h1>
+        <span className='big-text align-right'>
+          asdfjkl
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default App
