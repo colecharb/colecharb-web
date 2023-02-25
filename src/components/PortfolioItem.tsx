2e27d02b33a5dd9959411241c78329586929ac98
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-24 22:20:44 -0500

    portfolioItem component

---
+++ src/components/PortfolioItem.tsx
@@ -0,0 +1,34 @@
+import '../App.css'
+import { PortfolioItem } from '../types'
+
+export default function (item: PortfolioItem) {
+
+  return (
+    <>
+      <h3 className='portfolio-item-title'>
+        {item.title}
+        {/* <span className='portfolio-item-title subtitle'>
+          {' ' + item.subtitle}
+        </span> */}
+      </h3>
+      <div className="portfolio-item">
+
+        <div className="portfolio-column left">
+          <div className='image-container'>
+            {[1, 2, 3].map(n => (
+              <img src={`images/${item.title}/${n}.PNG`} width='30%' />
+            ))}
+          </div>
+
+
+        </div>
+
+        <div className="portfolio-column right">
+          <div className='portfolio-description'>
+            {item.description}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
