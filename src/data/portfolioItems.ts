eca37c1f422ff775c66addbba46bcf030ec76c7f
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-25 00:16:39 -0500

    updated descriptions

--- src/data/portfolioItems.ts
+++ src/data/portfolioItems.ts
@@ -4,12 +4,12 @@ const portfolioItems: PortfolioItem[] = [
   {
     title: "PocketChange",
     subtitle: "(iOS)",
-    description: "PocketChange for iOS is the consumer endpoint for participating in the PocketChange neighbourhood marketplaces in Toronto. The app is built in React Native and interfaces with SQL and MongoDB backend databases via GraphQL."
+    description: "PocketChange for iOS is the consumer endpoint for participating in the PocketChange neighbourhood marketplaces in Toronto. The app is built in React Native and uses GraphQL to interface with SQL and MongoDB backend databases."
   },
   {
     title: "ClearCast",
     subtitle: "(iOS)",
-    description: "ClearCast for iOS is a minimalist weather app featuring daily and bi-hourly forecasts with an intuitive UI. Data from the OpenWeather API is aggregated to display one cohesive screen of weather forecasts. Layout animations allow the single weather screen to smoothly and believably adapt to showing as much or little information as the user wants to see."
+    description: "ClearCast for iOS is a minimalist weather app featuring daily and bi-hourly forecasts. Data from the OpenWeather API is aggregated to display one intuitive screen of weather information. Layout animations allow the single weather screen to smoothly and believably adapt to showing as much or little information as the user needs. Demo available upon request."
   }
 ];
 export default portfolioItems;
