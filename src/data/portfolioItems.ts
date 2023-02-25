9a60d3e172ef6812eea6039f72c36c075839146a
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-24 22:20:54 -0500

    portfolio items data

---
+++ src/data/portfolioItems.ts
@@ -0,0 +1,15 @@
+import { PortfolioItem } from "../types";
+
+const portfolioItems: PortfolioItem[] = [
+  {
+    title: "PocketChange",
+    subtitle: "(iOS)",
+    description: "PocketChange partners with Toronto neighbourhoods and farmers' markets to bring zero-cost digital payment processing to their merchants. PocketChange empowers communities to meaningfully support their local economies, and provides local leaders with data-driven tools to create change."
+  },
+  {
+    title: "ClearCast",
+    subtitle: "(iOS)",
+    description: "ClearCast is a minimalist weather app for iOS with an emphasis on intuitive UX. asldfk sdlfk sd fsldf aslkdfj asldjs dfskdjf asld skdj sadl skdjfasdlkfj;a sdlfkjsa dfkjfkdjf;a slk dkjf slsjlaskdfj dfk sksj dfkfj asd.f akj fkj dfs. skfj alsdkjf asldkfjasdf;  asdlkfj 5j kfjfsldkj adfjaksdj f f falskdjf f."
+  }
+];
+export default portfolioItems;
