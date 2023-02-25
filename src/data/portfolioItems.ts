6b6797dab3f529bdcaa11b906cf3608b66ca24a2
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-25 00:02:28 -0500

    edited descriptions

--- src/data/portfolioItems.ts
+++ src/data/portfolioItems.ts
@@ -4,12 +4,12 @@ const portfolioItems: PortfolioItem[] = [
   {
     title: "PocketChange",
     subtitle: "(iOS)",
-    description: "PocketChange partners with Toronto neighbourhoods and farmers' markets to bring zero-cost digital payment processing to their merchants. PocketChange empowers communities to meaningfully support their local economies, and provides local leaders with data-driven tools to create change."
+    description: "PocketChange for iOS is the consumer endpoint for participating in the PocketChange neighbourhood marketplaces in Toronto. The app is built in React Native and interfaces with SQL and MongoDB backend databases via GraphQL."
   },
   {
     title: "ClearCast",
     subtitle: "(iOS)",
-    description: "ClearCast is a minimalist weather app for iOS with an emphasis on intuitive UI. asldfk sdlfk sd fsldf aslkdfj asldjs dfskdjf asld skdj sadl skdjfasdlkfj;a sdlfkjsa dfkjfkdjf;a slk dkjf slsjlaskdfj dfk sksj dfkfj asd.f akj fkj dfs. skfj alsdkjf asldkfjasdf;  asdlkfj 5j kfjfsldkj adfjaksdj f f falskdjf f."
+    description: "ClearCast for iOS is a minimalist weather app featuring daily and bi-hourly forecasts with an intuitive UI. Data from the OpenWeather API is aggregated to display one cohesive screen of weather forecasts. Layout animations allow the single weather screen to smoothly and believably adapt to showing as much or little information as the user wants to see."
   }
 ];
 export default portfolioItems;
