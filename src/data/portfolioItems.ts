53ac0d97a82de378f7aa1d35479fa5592f4954f1
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-26 01:00:06 -0500

    edited description

--- src/data/portfolioItems.ts
+++ src/data/portfolioItems.ts
@@ -1,15 +1,20 @@
 import { PortfolioItem } from "../types";
 
 const portfolioItems: PortfolioItem[] = [
+  // {
+  //   title: "This Site!",
+  //   // subtitle: "Web",
+  //   // description: ""
+  // },
   {
     title: "PocketChange",
     subtitle: "(iOS)",
-    description: "iOS application for participating in the PocketChange neighbourhood marketplaces in Toronto. The front end is built in React Native and uses GraphQL to interface with SQL and MongoDB databases."
+    description: "Application for engaging in the PocketChange digital neighbourhood marketplaces in Toronto. Built in React Native, the front end uses GraphQL to interface with SQL and MongoDB databases."
   },
   {
     title: "ClearCast",
     subtitle: "(iOS)",
-    description: "A minimalist weather app for iOS. Data from OpenWeather APIs are aggregated to display one intuitive screen of daily and bi-hourly forecasts. Layout animations allow this single screen to smoothly and convincingly adjust to show as much or little information as the user needs. Demo available upon request."
+    description: "A minimalist weather app for iOS. Data from OpenWeather APIs are aggregated to display one intuitive screen of daily and bi-hourly forecasts. Layout animations allow the app to smoothly and convincingly adjust to show as much or little information as the user needs. Demo available upon request."
   }
 ];
 export default portfolioItems;
