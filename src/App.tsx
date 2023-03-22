af325a6a079ceb2b2393bf2e3f9010d249f2d509
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-22 17:17:04 -0400

    move router into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-import SiteRouter from './SiteRouter';
+import About from './components/About';
+import ContactInfo from './components/ContactInfo';
+import Portfolio from './components/Portfolio';
+
+type Tab = { title: string, path: string };
 
 function App() {
+
+  const tabs: Tab[] = [
+    { title: "About", path: '/' },
+    { title: "Portfolio", path: '/portfolio' },
+    { title: "Contact", path: '/contact' },
+  ];
 
   return (
     <div className="App">
 
-      <SiteRouter />
+      <div className="line-horizontal" />
+
+      <div className="tab-bar">
+        {tabs.map((tab, index) => (
+          <h2>
+            <NavLink
+              to={tab.path}
+              className='tab-button'
+            >
+              {tab.title}
+            </NavLink>
+          </h2>
+        ))}
+      </div>
+
+      <div className="line-horizontal" />
+
+      <div className="section">
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<ContactInfo />} />
+        </Routes>
+      </div>
 
     </div>
   );
