9836455df1fa89e80dbe12ec44116376df548b8d
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-22 18:12:17 -0400

    updated about info

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ export default function () {
 
       <p className='big-text'>
         Full-stack engineer and UX designer.
-        Currently developing for iOS at PocketChange
+        Currently developing for iOS at PocketChange.
         {/* <a className='poch' href='https://pocketchangeapp.ca' target='_blank'>
           <span className='poch'>PocketChange</span>
         </a>. */}
@@ -18,10 +18,8 @@ export default function () {
         Developing intuitive and appealing user experiences is what I'm all about.
       </p>
       <p>
-
-        Please take a look at my portfolio for examples of my work and feel free to contact me if you're interested in working together.
+        Check out my portfolio for examples of my work and feel free to contact me if you're interested in working together.
         I'm always excited to take on new challenges.
-
       </p>
       <p>
         – Cole
