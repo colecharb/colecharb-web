00842c6c7a22a3cc7796426aeed87f3c6a354143
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-23 11:14:27 -0400

    pulled BusinessCard.tsx contents into Contact Page

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,7 +1,28 @@
-import BusinessCard from "./BusinessCard";
+import { FaLinkedin } from "react-icons/fa";
+import { GiEnvelope, GiRotaryPhone } from "react-icons/gi";
 
 export default function () {
   return (
-    <BusinessCard />
+    <div>
+      <div className='contact-item'>
+        <GiEnvelope className='contact-icon' />
+        <a href='mailto:colecharb.dev@gmail.com' target='_blank' rel="noopener noreferrer">
+          colecharb.dev@gmail.com
+        </a>
+      </div>
+
+      <div className='contact-item'>
+        <GiRotaryPhone className='contact-icon' />
+        <a href='tel:+14138541225'>+1 (413) 854-1225</a>
+      </div>
+
+      <div className='contact-item'>
+        <FaLinkedin className='contact-icon' />
+        <a href='https://www.linkedin.com/in/colecharbonneau/' target='_blank' rel="noopener noreferrer">
+          LinkedIn
+        </a>
+      </div>
+
+    </div>
   )
 }
