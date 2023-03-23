c0c983f84e1ff7cf5b88d793dd6a66635b543123
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-23 12:31:44 -0400

    renamed portfolioItems -> appProjects

---
+++ src/data/appProjects.ts
@@ -0,0 +1,16 @@
+import { App } from "../types";
+
+const appProjects: App[] = [
+  {
+    title: "PocketChange",
+    subtitle: "(iOS)",
+    abstract: "Application for engaging in the PocketChange digital neighbourhood marketplaces in Toronto. The app supports making and receiving payments, earning rewards, and participating in local contests."
+  },
+  {
+    title: "ClearCast",
+    subtitle: "(iOS)",
+    abstract: "A minimalist weather app for iOS designed around one intuitive screen of daily and bi-hourly forecasts. Layout animations allow the app to smoothly and convincingly adjust to show as much or little information as the user needs."
+  }
+];
+
+export default appProjects;
