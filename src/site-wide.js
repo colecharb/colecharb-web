bd7ec0ba9cb809278bf6366bc4cf410263156a31
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-26 14:56:11 -0400

    open all external links in new tab

---
+++ src/site-wide.js
@@ -0,0 +1,10 @@
+// Open external links in new tab (or window, based on browser preferences)
+window.addEventListener("DOMContentLoaded", function externalLinks() {
+  var anchors = document.getElementsByTagName("a");
+  for (var i = 0; i < anchors.length; i++) {
+    if (anchors[i].hostname !== window.location.hostname) {
+      anchors[i].setAttribute("target", "_blank");
+      anchors[i].setAttribute("rel", "noopener");
+    }
+  }
+});
