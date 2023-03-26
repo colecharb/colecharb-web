31b2d8bf4c3beff1df85ab3fa3528a099f54c3f1
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-26 14:57:18 -0400

    added github

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,25 +1,35 @@
 import { FaLinkedin } from "react-icons/fa";
 import { GiEnvelope, GiRotaryPhone } from "react-icons/gi";
+import { GoGitBranch } from "react-icons/go";
 
 export default function () {
   return (
     <div>
       <div className='contact-item'>
+        <GoGitBranch className='contact-icon' />
+        <a href='https://github.com/colecharb'>
+          GitHub
+        </a>
+      </div>
+
+      <div className='contact-item'>
+        <FaLinkedin className='contact-icon' />
+        <a href='https://www.linkedin.com/in/colecharbonneau/' >
+          LinkedIn
+        </a>
+      </div>
+
+      <div className='contact-item'>
         <GiEnvelope className='contact-icon' />
-        <a href='mailto:colecharb.dev@gmail.com' target='_blank' rel="noopener noreferrer">
+        <a href='mailto:colecharb.dev@gmail.com'>
           colecharb.dev@gmail.com
         </a>
       </div>
 
       <div className='contact-item'>
         <GiRotaryPhone className='contact-icon' />
-        <a href='tel:+14138541225'>+1 (413) 854-1225</a>
-      </div>
-
-      <div className='contact-item'>
-        <FaLinkedin className='contact-icon' />
-        <a href='https://www.linkedin.com/in/colecharbonneau/' target='_blank' rel="noopener noreferrer">
-          LinkedIn
+        <a href='tel:+14138541225'>
+          +1 (413) 854-1225
         </a>
       </div>
 
