77efc768c2c00e50b04aca0675e66c25e7e6b472
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-26 14:57:47 -0400

    added info about testflight and link to github

--- src/components/ProjectClearCast.tsx
+++ src/components/ProjectClearCast.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import appProjects from "../data/appProjects";
 import Project_App from "./Project_App";
 
@@ -5,7 +6,7 @@ export default function () {
   return (
     <Project_App app={appProjects.ClearCast}>
       <p>
-        I have always felt that most weather apps overcomplicate their UI;
+        I have always felt that most weather apps have overly-complicated UIs;
         with ClearCast, I focused on a minimalist interface built around the following tenets:
         <ul style={{ fontStyle: 'italic' }}>
           <li>
@@ -21,6 +22,10 @@ export default function () {
         Weather for the current day is extended back in time to display hourly forecasts from 12 A.M. through the end of the day.
         Integration with Google Places Autocomplete makes finding places effortless and familiar,
         and layout animations allow the app to smoothly and convincingly adjust to show as much or little information as the user needs.
+      </p>
+      <p>
+        ClearCast is currently available on TestFlight via invite. <Link to="/contact">Let me know</Link> if you'd like to be a tester!
+        Check out the <a href="https://github.com/colecharb/ClearCast">GitHub repo</a>.
       </p>
     </Project_App>
   )
