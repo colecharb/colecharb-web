d652cf0c662fce353c80eaa7e1fddd87b25f0d45
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-26 15:04:25 -0400

    trying another fix for external links in new tab

--- src/site-wide.js
+++ src/site-wide.js
@@ -1,10 +1,19 @@
 // Open external links in new tab (or window, based on browser preferences)
-window.addEventListener("DOMContentLoaded", function externalLinks() {
-  var anchors = document.getElementsByTagName("a");
-  for (var i = 0; i < anchors.length; i++) {
-    if (anchors[i].hostname !== window.location.hostname) {
-      anchors[i].setAttribute("target", "_blank");
-      anchors[i].setAttribute("rel", "noopener");
-    }
+// window.addEventListener("DOMContentLoaded", function externalLinks() {
+//   var anchors = document.getElementsByTagName("a");
+//   for (var i = 0; i < anchors.length; i++) {
+//     if (anchors[i].hostname !== window.location.hostname) {
+//       anchors[i].setAttribute("target", "_blank");
+//       anchors[i].setAttribute("rel", "noopener");
+//     }
+//   }
+// });
+
+function externalLinks() {
+  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+    var b = c[a];
+    b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
   }
-});
+}
+;
+externalLinks();
