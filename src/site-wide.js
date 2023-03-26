b4c91346154ff9416f17c870772d4e232df7ba65
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-26 15:20:30 -0400

    this should open external link in new tab

--- src/site-wide.js
+++ src/site-wide.js
@@ -1,10 +1,11 @@
 // Open external links in new tab (or window, based on browser preferences)
 
-
-function externalLinks() {
-  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
-    var b = c[a];
-    b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
+window.addEventListener("DOMContentLoaded", function externalLinks() {
+  var anchors = document.getElementsByTagName("a");
+  for (var i = 0; i < anchors.length; i++) {
+    if (anchors[i].hostname !== window.location.hostname) {
+      anchors[i].setAttribute("target", "_blank");
+      anchors[i].setAttribute("rel", "noopener");
+    }
   }
-};
-externalLinks();
+});
