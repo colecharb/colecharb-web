cebeac248a9503590850f17bea0d45d4446df3d7
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-26 15:07:02 -0400

    attempt #3 at external links open in new tab

--- src/site-wide.js
+++ src/site-wide.js
@@ -1,13 +1,5 @@
 // Open external links in new tab (or window, based on browser preferences)
-// window.addEventListener("DOMContentLoaded", function externalLinks() {
-//   var anchors = document.getElementsByTagName("a");
-//   for (var i = 0; i < anchors.length; i++) {
-//     if (anchors[i].hostname !== window.location.hostname) {
-//       anchors[i].setAttribute("target", "_blank");
-//       anchors[i].setAttribute("rel", "noopener");
-//     }
-//   }
-// });
+
 
 function externalLinks() {
   for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
@@ -16,4 +8,5 @@ function externalLinks() {
   }
 }
 ;
-externalLinks();
+
+window.addEventListener("DOMContentLoaded", externalLinks);
