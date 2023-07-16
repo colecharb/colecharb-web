7b711d38828db7b0b49ec1876814d8ebf165f63a
colecharb <40375497+colecharb@users.noreply.github.com>
2023-07-16 11:08:28 -0400

    updated info, added dash symbol as unicode

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -3,29 +3,30 @@ import { Link } from "react-router-dom";
 export default function () {
   return (
     <>
-      {/* <h2>About</h2> */}
-
-      <p className='big-text'>
-        Full-stack engineer and UX designer.
-        Currently developing for iOS at PocketChange.
-        {/* <a className='poch' href='https://pocketchangeapp.ca' target='_blank'>
-          <span className='poch'>PocketChange</span>
-        </a>. */}
+      <p className="big-text">
+        Full-stack engineer and UX designer. Currently developing web apps for{" "}
+        <a
+          className="employer"
+          href="https://certusinnovations.com"
+          target="_blank"
+        >
+          <span className="poch">Certus Innovations</span>
+        </a>
+        .
       </p>
 
       <p>
-        Hi!
-        I like making things.
-        I'm a developer and designer with a background in pure mathematics and an obsession with constantly learning and improving my skills.
-        Developing intuitive and appealing user experiences is what I'm all about.
+        Hi! I like making things. I'm a developer and designer with a background
+        in pure mathematics and an obsession with constantly learning and
+        improving my skills. Developing intuitive and appealing user experiences
+        is what I'm all about.
       </p>
       <p>
-        Check out the projects tab for examples of my work and feel free to contact me if you're interested in working together.
-        I'm always excited to take on new challenges.
+        Check out the projects tab for examples of my work and feel free to
+        contact me if you're interested in working together. I'm always excited
+        to take on new challenges.
       </p>
-      <p>
-        – Cole
-      </p>
+      <p>{"\u2013"} Cole</p>
     </>
-  )
+  );
 }
