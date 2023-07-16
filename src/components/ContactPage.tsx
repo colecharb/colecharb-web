160154ce931a9c73760a30eee82d38a2ed6d8566
colecharb <40375497+colecharb@users.noreply.github.com>
2023-07-16 11:07:57 -0400

    prettier going ham on quotes

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -5,34 +5,25 @@ import { GoGitBranch } from "react-icons/go";
 export default function () {
   return (
     <div>
-      <div className='contact-item'>
-        <GoGitBranch className='contact-icon' />
-        <a href='https://github.com/colecharb'>
-          GitHub
-        </a>
+      <div className="contact-item">
+        <GoGitBranch className="contact-icon" />
+        <a href="https://github.com/colecharb">GitHub</a>
       </div>
 
-      <div className='contact-item'>
-        <FaLinkedin className='contact-icon' />
-        <a href='https://www.linkedin.com/in/colecharbonneau/' >
-          LinkedIn
-        </a>
+      <div className="contact-item">
+        <FaLinkedin className="contact-icon" />
+        <a href="https://www.linkedin.com/in/colecharbonneau/">LinkedIn</a>
       </div>
 
-      <div className='contact-item'>
-        <GiEnvelope className='contact-icon' />
-        <a href='mailto:colecharb.dev@gmail.com'>
-          colecharb.dev@gmail.com
-        </a>
+      <div className="contact-item">
+        <GiEnvelope className="contact-icon" />
+        <a href="mailto:colecharb.dev@gmail.com">colecharb.dev@gmail.com</a>
       </div>
 
-      <div className='contact-item'>
-        <GiRotaryPhone className='contact-icon' />
-        <a href='tel:+14138541225'>
-          +1 (413) 854-1225
-        </a>
+      <div className="contact-item">
+        <GiRotaryPhone className="contact-icon" />
+        <a href="tel:+14138541225">+1 (413) 854-1225</a>
       </div>
-
     </div>
-  )
+  );
 }
