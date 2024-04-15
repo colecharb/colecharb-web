bf448c6f223fe584a159e72f1da4f997c82b4b5b
colecharb <colecharbonneau@gmail.com>
2024-04-15 11:47:15 -0400

    fix(about): updates web -> web and mobile and adjusts src newlines

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -2,7 +2,9 @@ export default function AboutPage() {
   return (
     <>
       <p className='big-text'>
-        Full-stack engineer and UX designer. Currently developing web apps at{' '}
+        Full-stack engineer and UX designer.
+        Currently developing for web and mobile at
+        {' '}
         <a
           className='employer'
           href='https://certusinnovations.com'
@@ -15,15 +17,15 @@ export default function AboutPage() {
       </p>
 
       <p>
-        Hi! I like making things. I'm a developer and designer with a background
-        in pure math and an obsession with constantly learning and improving my
-        skills. Developing intuitive and appealing user experiences is what I'm
-        all about.
+        Hi! I like making things.
+        I'm a developer and designer with a background in pure math
+        and an obsession with constantly learning and improving my skills.
+        Developing intuitive and appealing user experiences is what I'm all about.
       </p>
       <p>
-        Check out the projects tab for examples of my work and feel free to
-        contact me if you're interested in working together. I'm always excited
-        to take on new challenges.
+        Check out the projects tab for examples of my work
+        and feel free to contact me if you're interested in working together.
+        I'm always excited to take on new challenges.
       </p>
       <p>{'\u2013'} Cole</p>
     </>
