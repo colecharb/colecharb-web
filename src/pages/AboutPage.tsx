0df8b6a4048c81b90290e5247531782efa46ae69
colecharb <40375497+colecharb@users.noreply.github.com>
2025-09-24 07:41:04 -0400

    fix: removes "innovation" from landing text

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,21 +2,20 @@ export default function AboutPage() {
   return (
     <>
       <p className='big-text'>
-        Full-stack engineer and UX designer.
-        Building high-performance web and mobile applications
-        with a focus on user experience and design innovation.
+        Full-stack engineer and UX designer. Building high-performance web and
+        mobile applications with a focus on user experience and design.
       </p>
 
       <p>
-        Hi! I like making things.
-        I'm a developer and designer with a background in pure math
-        and an obsession with constantly learning and improving my skills.
-        Developing intuitive and appealing user experiences is what I'm all about.
+        Hi! I like making things. I'm a developer and designer with a background
+        in pure math and an obsession with constantly learning and improving my
+        skills. Developing intuitive and appealing user experiences is what I'm
+        all about.
       </p>
       <p>
-        Check out the projects tab for examples of my work
-        and feel free to contact me if you're interested in working together.
-        I'm always excited to take on new challenges.
+        Check out the projects tab for examples of my work and feel free to
+        contact me if you're interested in working together. I'm always excited
+        to take on new challenges.
       </p>
       <p>{'\u2013'} Cole</p>
     </>
